4c195231780b51517506009b3686812303a58208
Jerry Wingsky <139034471+JerryWingsky@users.noreply.github.com>
2025-04-23 20:37:47 +0700

    Create Level1.java

---
+++ Level1.java
@@ -0,0 +1,21 @@
+package gamePanel;
+
+import entity.player;
+import entity.zombie;
+
+
+public class Level1 extends Level {
+    public Level1(GamePanel gp) {
+        super(gp, 1);
+}
+
+    @Override
+    protected void initializeLevel() {
+        Player = new player(gp, gp.getKeyHandler(), 640, 256, "down");
+        zombies.add(new zombie(gp, Player, "moving", 640, 384, "right"));
+        zombies.add(new zombie(gp, Player, "static", 448, 256, "right"));
+
+        // Inserts the level tiles when you first load the level
+        Player.zombieCanMove = true;
+}
+}
