4a27b13b98d3cafe25592c5cbca0ffb21ad0a365
Jerry Wingsky <139034471+JerryWingsky@users.noreply.github.com>
2025-04-23 17:45:15 +0700

    Create entity.java

---
+++ entity.java
@@ -0,0 +1,13 @@
+package entity;
+
+import java.awt.image.BufferedImage;
+
+// The entity superclass so entities can use it to instantiate their variables
+public class entity {
+    public int x,y;
+    public int speed;
+    public BufferedImage up1, up2, left1, left2, right1, right2, down1, down2;
+    public String direction;
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+}
