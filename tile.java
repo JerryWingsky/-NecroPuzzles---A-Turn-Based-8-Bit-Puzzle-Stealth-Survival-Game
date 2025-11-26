5707c9ab5400c2b63ef446f4dc6b27e02c2cfd5f
Jerry Wingsky <139034471+JerryWingsky@users.noreply.github.com>
2025-11-26 20:05:07 +0800

    Update tile.java

--- tile.java
+++ tile.java
@@ -2,18 +2,18 @@ package tileCode;
 
 import java.awt.image.BufferedImage;
 
-// This is the superclass for tiles
+// This is the superclass for tiles.
 public class tile {
-    // This is an image
+    // This is an image.
     public BufferedImage image;
-    // This is collision for a tile
+    // This is a collision for a tile.
     public boolean collision = false;
-    // If the player stands here their death (i haven't touched it)
+    // If the player stands here, their death (I haven't touched it).
     public boolean death = false;
-    // If the player stands here they win
+    // If the player stands here, they win.
     public boolean victory = false;
-    // Checks for zombie path
+    // Checks for Zombie Path.
     public boolean zombiePath = false;
-    // Checks for player path
+    // Checks for Player Path.
     public boolean playerPath = false;
 }
