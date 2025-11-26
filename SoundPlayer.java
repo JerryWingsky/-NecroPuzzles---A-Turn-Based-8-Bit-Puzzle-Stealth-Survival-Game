8013092900cf9e1d9204b469e46e628dabf0d233
Jerry Wingsky <139034471+JerryWingsky@users.noreply.github.com>
2025-11-26 19:59:37 +0800

    Update SoundPlayer.java

--- SoundPlayer.java
+++ SoundPlayer.java
@@ -10,7 +10,7 @@ public class SoundPlayer {
 
     public SoundPlayer(String filePath) {
         try {
-            // Use class loader to load the music file
+            // Use Class Loader to load the Music File.
             URL soundURL = getClass().getResource(filePath);
             if (soundURL == null) {
                 System.out.println("Music file not found: " + filePath);
@@ -20,7 +20,7 @@ public class SoundPlayer {
             clip = AudioSystem.getClip();
             clip.open(audioStream);
 
-            // Try to get the volume control from the clip
+            // Try to get the Volume Control from the clip.
             if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
                 volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             } else {
@@ -33,26 +33,26 @@ public class SoundPlayer {
 
     public void play() {
         if (clip != null) {
-            clip.setFramePosition(0); // Rewind to the beginning
-            clip.start(); // Start playing
+            clip.setFramePosition(0); // Rewind to the beginning.
+            clip.start(); // Start playing.
 }
 }
 
     public void stop() {
         if (clip != null) {
-            clip.stop(); // Stop playing
+            clip.stop(); // Stop playing.
 }
 }
 
     public void loop() {
         if (clip != null) {
-            clip.loop(Clip.LOOP_CONTINUOUSLY); // Loop indefinitely
+            clip.loop(Clip.LOOP_CONTINUOUSLY); // Loop indefinitely.
 }
 }
 
     public void setVolume(float volume) {
         if (volumeControl != null) {
-            // volume should be a value between 0.0f (silent) and 1.0f (full volume)
+            // volume should be a value between 0.0f (Silent) & 1.0f (Full Volume).
             float min = volumeControl.getMinimum();
             float max = volumeControl.getMaximum();
             float newVolume = min + (max - min) * volume;
