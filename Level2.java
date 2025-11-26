35d9ba631668c117e05bcb9e62cebc33273e9e02
Jerry Wingsky <139034471+JerryWingsky@users.noreply.github.com>
2025-11-26 19:25:58 +0800

    Update Level2.java

--- Level2.java
+++ Level2.java
@@ -13,7 +13,7 @@ public class Level2 extends Level {
     @Override
     protected void initializeLevel() {
         Player = new player(gp, gp.getKeyHandler(), 640, 256, "down");
-        // Add specific zombies for level 2
+        // Add specific Zombies for Level 2.
         zombies.add(new zombie(gp, Player, "moving", 704, 512, "left", "left"));
         zombies.add(new zombie(gp, Player, "rotate", 576, 256, "left", "left"));
         zombies.add(new zombie(gp, Player, "moving", 512, 448, "right", "left"));
@@ -25,7 +25,7 @@ public class Level2 extends Level {
         spikes.add(new spike(gp, Player, "button", 384, 384, "up", buttons.get(0)));
         spikes.add(new spike(gp, Player, "button", 384, 448, "up", buttons.get(0)));
 
-        // Inserts the level tiles when you first load the level
+        // Inserts the Level tiles when you first load the Level.
         Player.zombieCanMove = true;
 }
 }
